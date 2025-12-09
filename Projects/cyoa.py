be7b3bcad4a52d69a466a079982d33cf2b5de9d5
TheOMGKraken <tristanearhart@seattleacademy.org>
2025-12-09 17:27:33 +0000

    project 1

---
+++ Projects/cyoa.py
@@ -0,0 +1,24 @@
+import time
+import sys
+
+mainvar = input("do you like chose your own adventures?")
+if mainvar != yes and mainvar != y and mainvar != yeah and mainvar != yep:
+    input("well thats too bad")
+mainvar = 0
+print("in this cyoa you will be asked a variety of questions")
+time.sleep(2)
+print("for every question, input 1 or 0")]
+time.sleep(2)
+print("understand?")
+time.sleep(0.1)
+print("good")
+time.sleep(1)
+print("if you are asked a yes or no question, 0 is no and 1 is yes")
+time.sleep(3)
+print("if you are asked a non yes or no question, 0 is the first option and 1 is the second option")
+time.sleep(4)
+print("lets get started!")
+time.sleep(3)
+input("you are in the 5th floor of the middle school and you hear that there is a zombie in the 4th floor of the upper school!")
+mainvar = input("do you go to 0: the stairs or 1: try to baracade the classroom")
+if mainvar != 1 and mainvar != 0
--- Projects/project_1.py
+++ Projects/project_1.py
@@ -1,20 +1,48 @@
+import sys
+import time
+
+"ttc means thing to change"
+"newfood means the new food"
+"google python read and write json file for persistent variables"
 input("press enter to start")
+input("welcome to the madlibs duck song!")
+badpeople = 0
+goodpeople = 0
 retry = 0
-duck = input("who should be the mc?")
-food = input(f"what should the {duck} want? (name a food)")
-supermarket = input(f"where could you get {food}")
-lemonade = input("what should the man be selling?")
+ttc = 0
+newfood = ""
+duck = input("who should be the mc? ")
+food = input(f"what should the {duck} want? (name a food) ")
+supermarket = input(f"where could you get {food} ")
+lemonade = input("what should the man be selling? ")
 if food == lemonade:
     while food == lemonade:
         
-        print("don't make what the {duck} wants and what the man sells the same thing")
-        input("")
-        ttc = input("which one do you want to change (type food or store)")
-        if ttc != "food" and ttc != "store":
-            input("I hate you")
-        if ttc == food:
+        if retry == 0:
+            input("don't make what the {duck} wants and what the man sells the same thing")
+        if retry == 1:
+            input("bruh")
+        if retry == 2:
+            input("is this funny for you?")
+        if retry == 3:
+            input("is this all just a game?")
+            input("why do you force me to do this?")
+        if retry == 4:
+            input("...")
+            input("no")
+            input("no more chances")
+            raise SystemError("i'm done with this")
+        ttc = 0
+        while ttc != "food" and ttc != "store":
+            newfood = ""
+            newfood = input("which one do you want to change (type food or store)")
+            while len(newfood) == 0:
+                newfood = input("which one do you want to change (type food or store)")
+            ttc = newfood
+        newfood = 0
+        if ttc == "food":
             food == input("what should the new food be")
-        if ttc == store:
+        if ttc == "store":
             lemonade == input("what should the new food be")
         retry += 1
 
@@ -100,7 +128,7 @@ input("erm, well actually no,")
 input(f"got any {food}?")
 input("the man sighed,")
 input("fine all right")
-input("why don't we go to the store")
+input(f"why don't we go to the {supermarket}")
 input(f"and get you some {food}")
 input("all right!")
 input("and they waddled away")
@@ -108,7 +136,7 @@ input("waddle waddle")
 input("and they waddled away")
 input("waddle waddle")
 input("and they waddled away")
-input("to the supermarket")
+input(f"to the {supermarket}")
 input("bum")
 input("bum")
 input("bum")
@@ -116,7 +144,7 @@ input("bum")
 input("bum")
 input("bum")
 input("bum")
-input("when they got to the {supermarket}, the man said")
+input(f"when they got to the {supermarket}, the man said")
 input(f"why don't we get you some {food}")
 input(f"actually, I am not really hungry for {food} anymore")
 input("but I could really go for some fresh, cold, homemade")
@@ -124,8 +152,34 @@ input(f"{lemonade}")
 
 print("and that's the duck song!")
 input("")
-yon = input("was that fun?")
-if yon != yes and yon != y and yon != yep and yon != aboslutly and yon != yeah:
-    print("bruh")
-    input("why?")
-    print("I honestly don't care in the slightest")
+yon = input("was that fun? (yes or no)")
+if yon != "yes" and yon != "y" and yon != "yep" and yon != "aboslutly" and yon != "yeah":
+    badpeople += 1
+    input("...")
+    input("...")
+    input("...")
+    raise SystemError("ಠ_ಠ")
+if yon == "yes" or yon == "y" or yon == "yep" or yon == "aboslutly" or yon == "yeah":
+    print(r"(/◕ヮ◕)/")
+    time.sleep(0.3)
+    print(r"\(◕ヮ◕\)")
+    time.sleep(0.3)
+    print(r"(/◕ヮ◕)/")
+    time.sleep(0.3)
+    print(r"\(◕ヮ◕\)")
+    time.sleep(0.3)
+    print(r"(/◕ヮ◕)/")
+    time.sleep(0.3)
+    print(r"\(◕ヮ◕\)")
+    time.sleep(0.3)
+    print(r"(/◕ヮ◕)/")
+    time.sleep(0.3)
+    print(r"\(◕ヮ◕\)")
+    time.sleep(0.3)
+    print(r"(/◕ヮ◕)/")
+    time.sleep(0.3)
+    print(r"\(◕ヮ◕\)")
+    time.sleep(0.3)
+    raise SystemError("＼(^o^)／")
+
+
